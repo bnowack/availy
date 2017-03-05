4882343970dd6dc830b5b775711cbb1f8f779240
Benjamin Nowack <mail@bnowack.de>
2017-03-05 14:16:18 +0100

    Init project (#setup)

---
+++ README.md
@@ -0,0 +1,3 @@
+# Availy
+
+Simple availability calendar
---
+++ src/Availy/Application/ApplicationController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Availy\Application;
+
+use Polymorph\Application\Application as Application;
+use Polymorph\Application\ApplicationController as PolymorphApplicationController;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Availy Application Controller
+ *
+ */
+class ApplicationController extends PolymorphApplicationController
+{
+
+}
