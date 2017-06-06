293f94f0a1ca4819b8e9bbc7e4a13183eb68440c
Benjamin Nowack <mail@bnowack.de>
2017-06-06 15:58:08 +0200

    Add support for multiple allocation files (#app)

--- src/Availy/Application/ApplicationController.php
+++ src/Availy/Application/ApplicationController.php
@@ -2,9 +2,8 @@
 
 namespace Availy\Application;
 
-use Polymorph\Application\Application as Application;
 use Polymorph\Application\ApplicationController as PolymorphApplicationController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  * Availy Application Controller
@@ -13,4 +12,19 @@ use Symfony\Component\HttpFoundation\Response;
 class ApplicationController extends PolymorphApplicationController
 {
 
+    /**
+     * Serves allocation definitions
+     *
+     * @param object $routeOptions
+     * @return JsonResponse
+     */
+    public function handleAllocationsRequest($routeOptions)
+    {
+        $response = [];
+        foreach ($routeOptions->files as $path) {
+            $data = json_decode(file_get_contents(POLYMORPH_APP_DIR . $path), true);
+            $response = array_merge($response, $data);
+        }
+        return new JsonResponse($response);
+    }
 }
